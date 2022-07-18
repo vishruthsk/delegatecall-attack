429373edb5e5fa3979ba3912e9d6c489e351234a
vishruthsk.eth <59907367+vishruthsk@users.noreply.github.com>
2022-07-18 18:23:17 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,29 @@
-const { expect } = require("chai");
-const { BigNumber } = require("ethers");
-const { ethers, waffle } = require("hardhat");
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
 
-describe("Attack", function () {
-  it("Should change the owner of the Good contract", async function () {
-    // Deploy the helper contract
-    const helperContract = await ethers.getContractFactory("helper");
-    const _helperContract = await helperContract.deploy();
-    await _helperContract.deployed();
-    console.log("Helper Contract's Address:", _helperContract.address);
+async function main() {
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-    // Deploy the good contract
-    const goodContract = await ethers.getContractFactory("good");
-    const _goodContract = await goodContract.deploy(_helperContract.address);
-    await _goodContract.deployed();
-    console.log("Good Contract's Address:", _goodContract.address);
+  const lockedAmount = hre.ethers.utils.parseEther("1");
 
-    // Deploy the Attack contract
-    const attackContract = await ethers.getContractFactory("attack");
-    const _attackContract = await attackContract.deploy(_goodContract.address);
-    await _attackContract.deployed();
-    console.log("Attack Contract's Address", _attackContract.address);
+  const Lock = await hre.ethers.getContractFactory("Lock");
+  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
-    // Now lets attack the good contract
+  await lock.deployed();
 
-    // Start the attack
-    let tx = await _attackContract.attack();
-    await tx.wait();
+  console.log("Lock with 1 ETH deployed to:", lock.address);
+}
 
-    expect(await _goodContract.owner()).to.equal(_attackContract.address);
-  });
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
 });
